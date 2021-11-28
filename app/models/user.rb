cda0243d37092f445a6ed8516dd069354a11426f
meena <meenabhagavathula100@gmail.com>
2021-11-28 15:55:38 +0530

    added css style to buttons for some pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   has_many :orders
   #validates :first_name, presence: true
   validates :email, { presence: true, uniqueness: true }
+
+  def is_owner?
+    role == "owner"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :menus
-  resources :orders
-  resources :menu_item
-  resources :users
-  resources :order_items
-  resources :active_menus
-
   get "/" => "home#index"
   get "/signin" => "sessions#new", as: :new_sessions
   post "/signin" => "sessions#create", as: :sessions
   delete "/signout" => "sessions#destroy", as: :destroy_session
+
+  resources :menus
+  resources :menu_item
+  resources :active_menus
+  resources :orders
+  resources :order_items
+  resources :users
+
   get "/user/orders/", to: "orders_user#index", as: "orders_user"
   get "/active/orders/", to: "orders_active#index", as: "orders_active"
   get "/user/completed/", to: "orders_user_completed#index", as: "orders_user_completed"
+  get "/owner/completed/", to: "orders_owner_completed#index", as: "orders_owner_completed"
   get "/mypanel/", to: "mypanel#index", as: "mypanel"
 
   #post "/" => "menus#create"

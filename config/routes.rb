d1d0dcf05d1cf837c8b21047f8f9cc1002fe3d51
meena <meenabhagavathula100@gmail.com>
2021-11-30 10:27:35 +0530

    routes problem with order resolved

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   resources :order_items
   resources :users
 
-  get "/users/orders/", to: "user_orders#index", as: "user_orders"
+  get "/user/orders/", to: "user_orders#index", as: "user_orders"
   get "/active/orders", to: "active_orders#index", as: "active_orders"
   get "/user/completed/", to: "user_completed_orders#index", as: "completed_orders"
   get "/owner/completed/", to: "owner_completed_orders#index", as: "owner_completed_orders"
